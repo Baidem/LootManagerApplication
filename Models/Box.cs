5eff7a63344e5f84c083916ac4d0db0485e36fb5
POUPAERT Vincent <baidem@gmail.com>
2023-02-16 11:12:26 +0100

    Modele Box

---
+++ Models/Box.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Box
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Label { get; set; }
+        // Navigation properties //
+        public Position Position { get; set; }
+        public int PositionId { get; set; }
+        public List<Loot> Loots { get; set; }
+    }
+}
