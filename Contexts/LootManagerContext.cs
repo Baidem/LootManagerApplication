0a0e0cc29c008e258c81e750c7f506943d4a52cf
POUPAERT Vincent <baidem@gmail.com>
2023-02-27 14:31:35 +0100

    Context DbSet Update

---
+++ Contexts/LootManagerContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace Contexts
+{
+    public class LootManagerContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Family> Families { get; set; }
+        public DbSet<House> Houses { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Storage> Storages { get; set; }
+        public DbSet<Shelf> Shelves { get; set; }
+        public DbSet<Position> Positions  { get; set; }
+        public DbSet<Element> Elements { get; set; }
+        public DbSet<InfoSheet> InfoSheets { get; set; }
+        public DbSet<Price> Prices { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Simple> Simples { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<BookSeries> BookSeriesList { get; set; }
+        public DbSet<Toy> Toys { get; set; }
+        public DbSet<ToySeries> ToySeriesList { get; set; }
+        public DbSet<Packaging> Packagings { get; set; }
+        public DbSet<Person> People { get; set; }
+    }
+}
--- Models/BookGenre.cs
+++ Models/BookGenre.cs
@@ -6,6 +6,6 @@ namespace Models
 
     public enum BookGenre
     {
-        Aventure, Classics, Crime, Fables, Fantasy, Roman, Drama, Historical_fiction, Horror, Humour_and_satire, Literary_fiction, Mystery, Poetry, Plays, Romance, Science_fiction, Short_stories, Thrillers, War, Women_s_fiction, Short_stories, Young_adult, Autobiography_and_memoir, Biography, Essays, Non_fiction_novel, Self_help, Superhero, Slice_of_Life, Humor, Non_fiction, Fantasy, Shonen, Seinen, Shojo, Josei, Yuri, Yaoi, Hentai, Fantasy, Comedy, Drama, Action, Other
+        Aventure, Classics, Crime, Fables, Fantasy, Roman, Drama, Historical_fiction, Horror, Humour_and_satire, Literary_fiction, Mystery, Poetry, Plays, Romance, Science_fiction, Short_stories, Thrillers, War, Women_s_fiction, Young_adult, Autobiography_and_memoir, Biography, Essays, Non_fiction_novel, Self_help, Superhero, Slice_of_Life, Humor, Non_fiction, Shonen, Seinen, Shojo, Josei, Yuri, Yaoi, Hentai, Comedy,  Action, Other
     }
 }
